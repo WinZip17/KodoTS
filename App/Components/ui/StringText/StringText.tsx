a2bf268e47ee57f1fccd74782e7a36a9a3181d8b
Юрий Аушев <wydo17@yandex.ua>
2020-09-18 16:33:47 +0300

    upd

--- App/Components/ui/StringText/StringText.tsx
+++ App/Components/ui/StringText/StringText.tsx
@@ -1,17 +1,20 @@
 import {Text, TextStyle} from 'react-native';
 import React from 'react';
 import styles from './StringText.styles';
+import isArray from 'lodash-es/isArray';
+
+type format =
+  | 'default'
+  | 'bold'
+  | 'defaultHeader'
+  | 'yellow'
+  | 'comments'
+  | 'green';
 
 type PropsType = {
   text: string;
   style?: TextStyle;
-  format?:
-    | 'default'
-    | 'bold'
-    | 'defaultHeader'
-    | 'yellow'
-    | 'comments'
-    | 'green';
+  format?: format | format[];
   fontSize?: null | 'p4' | 'p3' | 'p2' | 'p1';
   isUpperCase: boolean;
 };
@@ -25,24 +28,36 @@ const StringText = ({
 }: PropsType) => {
   const useStyles: TextStyle[] = [styles.stylesDefault];
 
-  switch (format) {
-    case 'bold':
-      useStyles.push(styles.stylesBold);
-      break;
-    case 'defaultHeader':
-      useStyles.push(styles.stylesDefaultHeader);
-      break;
-    case 'yellow':
-      useStyles.push(styles.stylesYellow);
-      break;
-    case 'comments':
-      useStyles.push(styles.stylesComments);
-      break;
-    case 'green':
-      useStyles.push(styles.stylesGreen);
-      break;
-    default:
-      useStyles.push(styles.stylesDefault);
+  const setStyleFormat = (stylesFormat: string) => {
+    switch (stylesFormat) {
+      case 'bold':
+        useStyles.push(styles.stylesBold);
+        break;
+      case 'defaultHeader':
+        useStyles.push(styles.stylesDefaultHeader);
+        break;
+      case 'yellow':
+        useStyles.push(styles.stylesYellow);
+        break;
+      case 'comments':
+        useStyles.push(styles.stylesComments);
+        break;
+      case 'green':
+        useStyles.push(styles.stylesGreen);
+        break;
+      default:
+        useStyles.push(styles.stylesDefault);
+    }
+  };
+
+  if (format && typeof format === 'string') {
+    setStyleFormat(format);
+  }
+
+  if (format && isArray(format)) {
+    format.forEach((i) => {
+      setStyleFormat(i);
+    });
   }
 
   if (fontSize) {
