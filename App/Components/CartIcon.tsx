744d7b39fa7c6677e53800e4348a15e7a4963c3a
Юрий Аушев <wydo17@yandex.ua>
2020-09-22 11:08:42 +0300

    upd cart

--- App/Components/CartIcon.tsx
+++ App/Components/CartIcon.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import icons from '../assets/icons';
+import {useSelector} from 'react-redux';
+import {RootState} from '../Stores/reducers';
+import map from 'lodash-es/map';
+import sum from 'lodash-es/sum';
+import property from 'lodash-es/property';
 
 const CartIcon: React.FC = (): JSX.Element => {
+  const itemsCount = useSelector((state: RootState) =>
+    sum(map(state.cart.items, property('count'))),
+  );
+
   return (
-    <>
-      <Text>
-          CartIcon.
-      </Text>
-    </>
+    <View>
+      <Image
+        source={icons.icCart}
+        style={[{width: 30, height: 30}]}
+        // tintColor={tintColor}
+      />
+      {itemsCount > 0 && (
+        <Text
+          style={{
+            color: '#fff',
+            backgroundColor: '#f00',
+            borderRadius: 8,
+            paddingTop: 2,
+            position: 'absolute',
+            width: 15,
+            height: 15,
+            fontSize: 8,
+            textAlign: 'center',
+            top: -3,
+            right: -3,
+            overflow: 'hidden',
+          }}>
+          {itemsCount}
+        </Text>
+      )}
+    </View>
   );
 };
 
--- App/Navigation/MenuAndroidCart.js
+++ App/Navigation/MenuAndroidCart.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Image, View, TouchableWithoutFeedback, StyleSheet} from 'react-native';
+import {Image, View, TouchableWithoutFeedback, StyleSheet, TouchableOpacity} from 'react-native';
 import icons from '../assets/icons';
 import {useSelector} from 'react-redux';
+import CartIcon from "../Components/CartIcon";
 
 const styles = StyleSheet.create({
   block: {
@@ -14,15 +15,12 @@ const styles = StyleSheet.create({
 });
 
 const MenuAndroidCart = ({navigation}) => {
-  const isCart = useSelector(({cart}) =>
-    cart.items && cart.items.length > 0 ? icons.icCart : icons.icCartAndroid,
-  );
   return (
     <View style={styles.block}>
       <TouchableWithoutFeedback
         onPress={() => navigation.navigate('CartScreen')}
         hitSlop={{top: 30, bottom: 30, left: 30, right: 30}}>
-        <Image source={isCart} style={styles.image} />
+        <CartIcon />
       </TouchableWithoutFeedback>
     </View>
   );
